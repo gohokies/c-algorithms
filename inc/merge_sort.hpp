9013db72e122b8db9f0e2c75c6cad27461fcd97b
Fabin Shen <shenfb@gmail.com>
2023-07-05 22:50:46 -0700

    more test for mergesort

--- inc/merge_sort.hpp
+++ inc/merge_sort.hpp
@@ -58,27 +58,13 @@ namespace algorithms
 
         // Divide source items to 4 parts, and sort each of them.
         RandomIt mid = begin + (count >> 1);
-        RandomIt auxMid = auxBegin + (count >> 1);
-        size_t lcount = mid - begin;
-        size_t rcount = end - mid;
-        RandomIt lmid = begin + (lcount >> 1);
-        RandomIt rmid = mid + (rcount >> 1);
-        RandomIt aux = auxBegin;
-        _merge_sort(begin, lmid, cmp, aux);
-        aux += lcount >> 1;
-        _merge_sort(lmid, mid, cmp, aux);
-        aux = auxMid;
-        _merge_sort(mid, rmid, cmp, aux);
-        aux += rcount >> 1;
-        _merge_sort(rmid, end, cmp, aux);
+        RandomIt auxmid = auxBegin + (count >> 1);
+        _merge_sort(begin, mid, cmp, auxBegin);
+        _merge_sort(mid, end, cmp, auxmid);
 
-        // Merge 4 sorted parts to auxiliray array, and merge back in place. 
-        //_merge_(begin, lmid, mid, cmp, auxBegin);
-        //_merge_(mid, rmid, end, cmp, auxMid);
         //_merge_(auxBegin, auxMid, auxBegin + count, cmp, begin);        
-        std::merge(begin, lmid, lmid, mid, auxBegin, cmp);
-        std::merge(mid, rmid, rmid, end, auxMid, cmp);
-        std::merge(auxBegin, auxMid, auxMid, auxBegin + count, begin, cmp);
+        std::merge(begin, mid, mid, end, auxBegin, cmp);
+        std::move(auxBegin, auxBegin + count, begin);
     }
 
     // Use auxiliry buffer to sort [begin, end). Caller needs to make sure that the 
@@ -137,6 +123,73 @@ namespace algorithms
         typedef typename std::iterator_traits<RandomIt>::value_type T;
         merge_sort_inplace(begin, end, std::less<T>());
     }    
+    template<typename RandomIt, typename Compare>
+    void _quad_sort(RandomIt begin, RandomIt end, Compare cmp, RandomIt auxBegin)
+    {
+        typedef typename std::iterator_traits<RandomIt>::value_type T;
+
+        size_t count = end - begin;
+        if (count < 2) return;
+
+        // Switch to insertion sort threshold.
+        if (count < kInsertSortThreshold)
+        {
+            insertion_sort(begin, end, cmp);
+            return;
+        }
+
+        // Divide source items to 4 parts, and sort each of them.
+        RandomIt mid = begin + (count >> 1);
+        RandomIt auxMid = auxBegin + (count >> 1);
+        size_t lcount = mid - begin;
+        size_t rcount = end - mid;
+        RandomIt lmid = begin + (lcount >> 1);
+        RandomIt rmid = mid + (rcount >> 1);
+        RandomIt aux = auxBegin;
+        _merge_sort(begin, lmid, cmp, aux);
+        aux += lcount >> 1;
+        _merge_sort(lmid, mid, cmp, aux);
+        aux = auxMid;
+        _merge_sort(mid, rmid, cmp, aux);
+        aux += rcount >> 1;
+        _merge_sort(rmid, end, cmp, aux);
+
+        // Merge 4 sorted parts to auxiliray array, and merge back in place. 
+        //_merge_(begin, lmid, mid, cmp, auxBegin);
+        //_merge_(mid, rmid, end, cmp, auxMid);
+        //_merge_(auxBegin, auxMid, auxBegin + count, cmp, begin);        
+        std::merge(begin, lmid, lmid, mid, auxBegin, cmp);
+        std::merge(mid, rmid, rmid, end, auxMid, cmp);
+        std::merge(auxBegin, auxMid, auxMid, auxBegin + count, begin, cmp);
+    }
+
+    // Use auxiliry buffer to sort [begin, end). Caller needs to make sure that the 
+    // auxiliry buffer must no less than the number of the items to sort. The
+    // sorted array is in place in [begin, end).
+    template<typename RandomIt, typename Compare>
+    void quad_sort(RandomIt begin, RandomIt end, Compare cmp, RandomIt dstBegin)
+    {
+        _quad_sort(begin, end, cmp, dstBegin);
+    }
+
+    // Allocate auxiliary buffer, the use it to do merge sort.
+    template<typename RandomIt, typename Compare>
+    void quad_sort(RandomIt begin, RandomIt end, Compare cmp)
+    {
+        typedef typename std::iterator_traits<RandomIt>::value_type T;
+        std::vector<T> buffer;
+        size_t count = (end - begin);
+    
+        buffer.resize(count);
+        _quad_sort(begin, end, cmp, buffer.begin());
+    }
+
+    template<typename RandomIt>
+    void quad_sort(RandomIt begin, RandomIt end)
+    {
+        typedef typename std::iterator_traits<RandomIt>::value_type T;
+        quad_sort(begin, end, std::less<T>());
+    }
 }
 
 #endif
--- test/UnitTest/MergeSortTest/MergeSortTest.cpp
+++ test/UnitTest/MergeSortTest/MergeSortTest.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 using namespace algorithms;
 
-template<typename T, bool ascend, size_t N>
-void MergeSortTestAndVerify(T delta)
+template<typename T, bool ascend>
+void MergeSortTestAndVerify(size_t N, T delta)
 {
     std::vector<T> v1, v2;
 
@@ -42,8 +42,8 @@ void MergeSortTestAndVerify(T delta)
     ASSERT_TRUE(v1 == v2);
 }
 
-template<typename T, bool ascend, size_t N>
-void MergeSortInplaceTestAndVerify(T delta)
+template<typename T, bool ascend>
+void MergeSortInplaceTestAndVerify(size_t N, T delta)
 {
     std::vector<T> v1, v2;
 
@@ -75,79 +75,242 @@ void MergeSortInplaceTestAndVerify(T delta)
     ASSERT_TRUE(v1 == v2);
 }
 
+template<typename T, bool ascend>
+void QuadSortTestAndVerify(size_t N, T delta)
+{
+    std::vector<T> v1, v2;
+
+    // Fill array
+    v1.resize(N);
+    Test::fill_random_values<T>(v1.data(), v1.size(), delta);
+
+    // Clone the array
+    v2 = v1;
+
+    if (ascend)
+    {
+        // Sort one array with std::sort
+        std::sort(v1.begin(), v1.end(), std::less<T>());
+
+        // Sort the other array with insertion sort
+        algorithms::quad_sort(v2.begin(), v2.end(), std::less<T>());
+    }
+    else
+    {
+        // Sort one array with std::sort in descending order
+        std::sort(v1.begin(), v1.end(), std::greater_equal<T>());
+
+        // Sort the other array with insertion sort in descending order
+        algorithms::quad_sort(v2.begin(), v2.end(), std::greater_equal<T>());
+    }
+
+    // Verify the two array should be the same
+    ASSERT_TRUE(v1 == v2);
+}
+
 //
-// Test unsigned integers
+// MergeSort: Test unsigned integers
 //
 TEST(MergeSortTest, MergeSortUint32)
 {
-    MergeSortTestAndVerify<uint32_t, true, 1024>(0);
+    MergeSortTestAndVerify<uint32_t, true>(1024, 0);
 }
 
 TEST(MergeSortTest, MergeSortUint32InDescendingOrder)
 {
-    MergeSortTestAndVerify<uint32_t, false, 1024>(0);
+    MergeSortTestAndVerify<uint32_t, false>(1024, 0);
 }
 
 TEST(MergeSortTest, MergeSortUint64)
 {
-    MergeSortTestAndVerify<uint64_t, true, 1024>(0);
+    MergeSortTestAndVerify<uint64_t, true>(1024, 0);
 }
 
 TEST(MergeSortTest, MergeSortUint64InDescendingOrder)
 {
-    MergeSortTestAndVerify<uint64_t, false, 1024>(0);
+    MergeSortTestAndVerify<uint64_t, false>(1024, 0);
 }
 
 //
-// Test signed integers
+// MergeSort: Test signed integers
 //
 TEST(MergeSortTest, MergeSortInt32)
 {
-    MergeSortTestAndVerify<int32_t, true, 1024>(RAND_MAX >> 1);
+    MergeSortTestAndVerify<int32_t, true>(1024, RAND_MAX >> 1);
 }
 
 TEST(MergeSortTest, MergeSortInt32InDescendingOrder)
 {
-    MergeSortTestAndVerify<int32_t, false, 1024>(RAND_MAX >> 1);
+    MergeSortTestAndVerify<int32_t, false>(1024, RAND_MAX >> 1);
 }
 
 TEST(MergeSortTest, MergeSortInt64)
 {
-    MergeSortTestAndVerify<int64_t, true, 1024>(RAND_MAX >> 1);
+    MergeSortTestAndVerify<int64_t, true>(1024, RAND_MAX >> 1);
 }
 
 TEST(MergeSortTest, MergeSortInt64InDescendingOrder)
 {
-    MergeSortTestAndVerify<int64_t, false, 1024>(RAND_MAX >> 1);
+    MergeSortTestAndVerify<int64_t, false>(1024, RAND_MAX >> 1);
 }
 
 //
-// Test floating points
+// MergeSort: Test floating points
 //
 TEST(MergeSortTest, MergeSortFloat)
 {
-    MergeSortTestAndVerify<float, true, 1024>(static_cast<float>(RAND_MAX >> 1));
+    MergeSortTestAndVerify<float, true>(1024, static_cast<float>(RAND_MAX >> 1));
 }
 
 TEST(MergeSortTest, MergeSortFloatInDescendingOrder)
 {
-    MergeSortTestAndVerify<float, false, 1024>(static_cast<float>(RAND_MAX >> 1));
+    MergeSortTestAndVerify<float, false>(1024, static_cast<float>(RAND_MAX >> 1));
 }
 
 TEST(MergeSortTest, MergeSortDouble)
 {
-    MergeSortTestAndVerify<double, true, 1024>(static_cast<double>(RAND_MAX >> 1));
+    MergeSortTestAndVerify<double, true>(1024, static_cast<double>(RAND_MAX >> 1));
 }
 
 TEST(MergeSortTest, MergeSortDoubleInDescendingOrder)
 {
-    MergeSortTestAndVerify<double, false, 1024>(static_cast<double>(RAND_MAX >> 1));
+    MergeSortTestAndVerify<double, false>(1024, static_cast<double>(RAND_MAX >> 1));
 }
 
 //
-// Test sorting unsigned integers
+// MergeSortInplace: Test unsigned integers
 //
-TEST(MergeSortTest, MergeSortInplaceUint32)
+TEST(MergeSortInplaceTest, MergeSortInplaceUint32)
 {
-    MergeSortInplaceTestAndVerify<uint32_t, true, 1024>(0);
+    MergeSortInplaceTestAndVerify<uint32_t, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceUint32InDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<uint32_t, false>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceUint64)
+{
+    MergeSortInplaceTestAndVerify<uint64_t, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceUint64InDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<uint64_t, false>(1024, 0);
+}
+
+//
+// MergeSortInplace: Test signed integers
+//
+TEST(MergeSortInplaceTest, MergeSortInplaceInt32)
+{
+    MergeSortInplaceTestAndVerify<int32_t, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceInt32InDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<int32_t, false>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceInt64)
+{
+    MergeSortInplaceTestAndVerify<int64_t, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceInt64InDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<int64_t, false>(1024, 0);
+}
+
+//
+// MergeSortInplace: Test floating points
+//
+TEST(MergeSortInplaceTest, MergeSortInplaceFloat)
+{
+    MergeSortInplaceTestAndVerify<float, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceFloatInDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<float, false>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceDouble)
+{
+    MergeSortInplaceTestAndVerify<double, true>(1024, 0);
+}
+
+TEST(MergeSortInplaceTest, MergeSortInplaceDoubleInDescendingOrder)
+{
+    MergeSortInplaceTestAndVerify<double, false>(1024, 0);
+}
+
+//
+// QuadSort: Test unsigned integers
+//
+TEST(QuadSortTest, QuadSortUint32)
+{
+    QuadSortTestAndVerify<uint32_t, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortUint32InDescendingOrder)
+{
+    QuadSortTestAndVerify<uint32_t, false>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortUint64)
+{
+    QuadSortTestAndVerify<uint64_t, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortUint64InDescendingOrder)
+{
+    QuadSortTestAndVerify<uint64_t, false>(1024, 0);
+}
+
+//
+// QuadSort: Test signed integers
+//
+TEST(QuadSortTest, QuadSortInt32)
+{
+    QuadSortTestAndVerify<int32_t, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortInt32InDescendingOrder)
+{
+    QuadSortTestAndVerify<int32_t, false>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortInt64)
+{
+    QuadSortTestAndVerify<int64_t, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortInt64InDescendingOrder)
+{
+    QuadSortTestAndVerify<int64_t, false>(1024, 0);
+}
+
+//
+// QuadSort: Test floating points
+//
+TEST(QuadSortTest, QuadSortFloat)
+{
+    QuadSortTestAndVerify<float, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortFloatInDescendingOrder)
+{
+    QuadSortTestAndVerify<float, false>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortDouble)
+{
+    QuadSortTestAndVerify<double, true>(1024, 0);
+}
+
+TEST(QuadSortTest, QuadSortDoubleInDescendingOrder)
+{
+    QuadSortTestAndVerify<double, false>(1024, 0);
 }
