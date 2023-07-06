5ff1cba315a80ebc134db5304971acd889b62b7f
Fabin Shen <shenfb@gmail.com>
2023-07-05 18:09:52 -0700

    update radixsort test

--- test/UnitTest/RadixSortTest/RadixSortTest.cpp
+++ test/UnitTest/RadixSortTest/RadixSortTest.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "gtest/gtest.h"
 
-#include "radixsort.h"
+#include "radix_sort.hpp"
 #include "TestHelpers.h"
 
 using namespace Test;
 
 template<typename T>
-void CompareRadixSortAndStdSort(std::vector<T> &v1, std::vector<T> &v2, RadixType type)
+void CompareRadixSortAndStdSort(std::vector<T> &v1, std::vector<T> &v2)
 {
     ASSERT_TRUE(v1.size() == v2.size());
 
@@ -15,53 +15,53 @@ void CompareRadixSortAndStdSort(std::vector<T> &v1, std::vector<T> &v2, RadixTyp
     std::sort(v1.begin(), v1.end());
 
     // Sort the other array with Radix sort
-    radix_sort_type_array(v2.data(), v2.size(), sizeof(T), type);
+    algorithms::radix_sort(v2.begin(), v2.end());
 
     // Verify the two array should be the same
     ASSERT_TRUE(v1 == v2);
 }
 
 template<typename T>
-void RadixSortTestAndVerify(T delta, size_t N, RadixType type)
+void RadixSortTestAndVerify(T delta, size_t N)
 {
     std::vector<T> v1, v2;
 
     // Fill array
     v1.resize(N);
-    fill_random_values<T>(v1.data(), v1.size(), delta);
+    Test::fill_random_values<T>(v1.data(), v1.size(), delta);
 
     // Clone the array
     v2 = v1;
 
-    CompareRadixSortAndStdSort<T>(v1, v2, type);
+    CompareRadixSortAndStdSort<T>(v1, v2);
 }
 
 TEST(RadixSortTest, RadixSortTest_uint32)
 {
-    RadixSortTestAndVerify<uint32_t>(0, 4096, t_uint32);
+    RadixSortTestAndVerify<uint32_t>(0, 4096);
 }
 
 TEST(RadixSortTest, RadixSortTest_int32)
 {
-    RadixSortTestAndVerify<int32_t>(RAND_MAX >> 1, 4096, t_int32);
+    RadixSortTestAndVerify<int32_t>(RAND_MAX >> 1, 4096);
 }
 
 TEST(RadixSortTest, RadixSortTest_uint64)
 {
-    RadixSortTestAndVerify<uint64_t>(0, 4096, t_uint64);
+    RadixSortTestAndVerify<uint64_t>(0, 4096);
 }
 
 TEST(RadixSortTest, RadixSortTest_int64)
 {
-    RadixSortTestAndVerify<int64_t>(RAND_MAX >> 1, 4096, t_int64);
+    RadixSortTestAndVerify<int64_t>(RAND_MAX >> 1, 4096);
 }
 
 TEST(RadixSortTest, RadixSortTest_float)
 {
-    RadixSortTestAndVerify<float>(RAND_MAX >> 1, 4096, t_float);
+    RadixSortTestAndVerify<float>(RAND_MAX >> 1, 4096);
 }
 
 TEST(RadixSortTest, RadixSortTest_double)
 {
-    RadixSortTestAndVerify<double>(RAND_MAX >> 1, 4096, t_double);
+    RadixSortTestAndVerify<double>(RAND_MAX >> 1, 4096);
 }
